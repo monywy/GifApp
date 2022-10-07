24da1dfe6c7801e6a1498009959c2d9ad2241664
monywy <44063035+monywy@users.noreply.github.com>
2022-10-07 13:58:53 -0500

    1 file modified

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -14,6 +14,10 @@ export class GifsService {
   buscarGifs(query: string){
 
     this._historial.unshift(query);
+    //Validacion elementos repetidos
+    if( !this._historial.indexOf( query ) ) {
+      this._historial.unshift( query );
+    }
     ///Limitar el numero de registros en el historial
     this._historial=this._historial.splice(0,10); 
     console.log(this._historial);
