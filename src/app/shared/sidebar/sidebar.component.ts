782311239c863edc2e6c8bd04062363effd3916e
monywy <44063035+monywy@users.noreply.github.com>
2022-10-07 13:31:17 -0500

    2 files modified

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  get historial(){
+    return this.gifsServices.historial;
+  }
+
+  constructor(private gifsServices:GifsService) { }
 
   ngOnInit() {
   }
