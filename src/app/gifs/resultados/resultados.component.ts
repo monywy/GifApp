ac9d655f90c64d31804457a6c5df27ea264c0bfe
monywy <44063035+monywy@users.noreply.github.com>
2022-10-07 17:22:28 -0500

    2 files modified

--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   
 })
-export class ResultadosComponent implements OnInit {
+export class ResultadosComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  get resultados(){
+    return this.gifServices.resultados;
   }
 
+  constructor(private gifServices:GifsService) { }
+
+ 
 }
