0058ef55b991a19ba9b681ba0faf761a842b744a
monywy <44063035+monywy@users.noreply.github.com>
2022-10-07 13:09:43 -0500

    1 file modified

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { __values } from 'tslib';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,19 +10,13 @@ import { __values } from 'tslib';
 export class BusquedaComponent {
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor(private gifsServices:GifsService){}
 
 
   buscar(){
 
-    
     const valor = this.txtBuscar.nativeElement.value;
-
-    //if ( valor.trim().length === 0 ) {
-     // return;
-    //}
-
-   // this.gifsService.buscarGifs( valor );
-
+    this.gifsServices.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
     console.log(valor);
 
