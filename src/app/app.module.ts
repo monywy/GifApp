65381e3ea9e0b7bd5f4498af555d856bfb69b2a2
monywy <44063035+monywy@users.noreply.github.com>
2022-10-07 10:35:04 -0500

    4 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, SharedModule,GifsModule],
+  declarations: [AppComponent, HelloComponent,SidebarComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
