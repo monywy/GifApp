f2fb9e519a7ab1d870a48a13a60d6de1fb5155b9
monywy <44063035+monywy@users.noreply.github.com>
2022-10-08 15:40:53 -0500

    1 file modified

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -29,10 +29,10 @@ export class GifsService {
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
     this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
   
-
+    //segundo método para traer información del LocalStorage
     // if( localStorage.getItem('historial') ){
     //   this._historial = JSON.parse( localStorage.getItem('historial')! );
-    // }
+   //  }
 
   }
 
