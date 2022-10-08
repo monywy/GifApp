a346dabe6ba49ba82685f9a5fef47ba9639f2a28
monywy <44063035+monywy@users.noreply.github.com>
2022-10-08 11:57:52 -0500

    3 files modified

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -20,26 +20,47 @@ export class GifsService {
 
     query=query.trim().toLocaleLowerCase();
 
+    for(let i; i<query.length;i++){
+      if(query.indexOf(query.charAt(i),0)!=-1){
+        this._historial.unshift( query );
+        this._historial=this._historial.splice(0,10); 
+
+      }
+
+    }
+
     //Validacion elementos repetidos
     
-    if( !this._historial.includes(query) ) {
-      this._historial.unshift( query );
-       ///Limitar el numero de registros en el historial
-      this._historial=this._historial.splice(0,10); 
-    }
+  ///  if( !this._historial.includes(query) ) {
+  ///  this._historial.unshift( query );
+     ///Limitar el numero de registros en el historial
+  ///    this._historial=this._historial.splice(0,10); 
+  ///  }
+
+    
+
+
     //Peticion get utilizando HttpClient
     //Se le pasa el valor de query a la busqueda
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q=${ query }&limit=10`)
+ 
+
+    //https://api.openweathermap.org/data/2.5/weather?lat=21.137272&lon=-86.878387&appid=3b81673b011be25a738bf92fed18062a
+    
+   this.http.get('https://rest-api-angular-php--parzibyte.repl.co/obtenerPersonas.php');
+
+ 
+    //this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q=${ query }&limit=10`)
     //Respuesta de la petición completa
    //.subscribe (resp =>{console.log(resp);}); 
 
    //Respuesta de la petición obteniendo unicamente la data
 
-   .subscribe((respdata:any)=>{
 
-    console.log(respdata.data);
-    this.resultados = respdata;
-   })
+   //.subscribe((respdata:any)=>{
+
+ //   console.log(respdata.data);
+  //  this.resultados = respdata;
+    // })
     
 
   }
