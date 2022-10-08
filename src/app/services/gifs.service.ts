e7b1fe7ba1c455e210c4e2d0c3a63dc22288a860
monywy <44063035+monywy@users.noreply.github.com>
2022-10-08 13:16:40 -0500

    2 files modified

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -10,32 +10,26 @@ export class GifsService {
 
   // Arreglo para guardar la respuesta.
   public resultados: any[] = [];
+  
 
   get historial() {
     return [...this._historial];
   }
   constructor(private http:HttpClient){}
 
+ 
+
   buscarGifs(query: string){
 
     query=query.trim().toLocaleLowerCase();
 
-    for(let i; i<query.length;i++){
-      if(query.indexOf(query.charAt(i),0)!=-1){
-        this._historial.unshift( query );
-        this._historial=this._historial.splice(0,10); 
-
-      }
-
-    }
-
     //Validacion elementos repetidos
     
-  ///  if( !this._historial.includes(query) ) {
-  ///  this._historial.unshift( query );
-     ///Limitar el numero de registros en el historial
-  ///    this._historial=this._historial.splice(0,10); 
-  ///  }
+    if( !this._historial.includes(query) ) {
+    this._historial.unshift( query );
+    //Limitar el numero de registros en el historial
+      this._historial=this._historial.splice(0,10); 
+    }
 
     
 
@@ -46,21 +40,22 @@ export class GifsService {
 
     //https://api.openweathermap.org/data/2.5/weather?lat=21.137272&lon=-86.878387&appid=3b81673b011be25a738bf92fed18062a
     
-   this.http.get('https://rest-api-angular-php--parzibyte.repl.co/obtenerPersonas.php');
+   // this.http.get('https://rest-api-angular-php--parzibyte.repl.co/obtenerPersonas.php');
 
  
-    //this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q=${ query }&limit=10`)
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q=${ query }&limit=10`)
     //Respuesta de la petición completa
    //.subscribe (resp =>{console.log(resp);}); 
 
    //Respuesta de la petición obteniendo unicamente la data
 
 
-   //.subscribe((respdata:any)=>{
+   .subscribe((respdata:any)=>{
 
- //   console.log(respdata.data);
-  //  this.resultados = respdata;
-    // })
+     console.log(respdata.data);
+     this.resultados = respdata;
+     })
     
 
   }
